90c71b80c808d43c720461ce378fc8466e1da874
Demillet GOKABA <demillet03@gmail.com>
2022-11-21 12:06:16 +0100

    installation scss/sass

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
 <div id="app">
-  
+  <RestaurantCard/>
 </div>
 </template>
 
 <script>
-
+import RestaurantCard from './components/RestaurantCard.vue'
 export default {
   name: 'App',
+  components:  {
+    RestaurantCard
+  }
 }
 </script>
 
