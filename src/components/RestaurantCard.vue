ac99d8a5d3f9b3fe3a1757603cdc17f19cce7807
Demillet GOKABA <demillet03@gmail.com>
2022-11-21 12:49:03 +0100

    creation du component RestaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="restaurant--card">
-    <p>test</p>
+    <div class="restaurant--image">
+        
+    </div>
+    <div class="restaurant--information">
+        <div class="details">
+            <p class="name">
+                Subway
+            </p>
+            <p class="note">
+                <span>4.5</span>
+            </p>
+        </div>
+        <p class="time">
+            20-30min
+        </p>
+    </div>
   </div>
 </template>
 
@@ -18,6 +33,35 @@ export default {
 
     p {
         margin: 0px;
+    }
+
+    .restaurant--image {
+        background-image: url("https://cn-geo1.uber.com/image-proc/resize/eats/format=webp/width=550/height=440/quality=70/srcb64=aHR0cHM6Ly9kMXJhbHNvZ25qbmczNy5jbG91ZGZyb250Lm5ldC8zNzg4MDJiMC1jNTI4LTQ4MjktYjBiNS0wY2M2NDBkZjYzY2QuanBlZw==");
+        height: 70%;
+        background-size: cover;        
+        background-position:center;  
+    }
+    .restaurant--information {
+        .details {
+            margin: 10px 0px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            .note {
+                background-color: #eeeeee;
+                height: 30px;
+                width: 30px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-size: 0.8rem;
+                border-radius: 50%;
+            }
+        }
+    }
+    .time {
+        font-size: 0.9rem;
     }
 }
 </style>
