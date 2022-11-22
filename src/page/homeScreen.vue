e69ca2a63b82407c8a41b24152beeca35613610c
Demillet GOKABA <demillet03@gmail.com>
2022-11-22 12:22:35 +0100

    Affichage de la fenêtre des résultats de recherche

--- src/page/homeScreen.vue
+++ src/page/homeScreen.vue
@@ -2,7 +2,20 @@
   <div class="homescreen">
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
-      <input v-model="user_search_restaurant" type="text" placeholder="De quoi avez-vous envie ?">
+
+      <div class="wrapper--input">
+        <input v-model="user_search_restaurant" type="text" placeholder="De quoi avez-vous envie ?">
+        <div class="search">
+          <div v-for="(restaurant, i) in search_restaurant" :key="i" class="container--restaurant--search">
+            <div class="wrapper--img">
+              <img :src="restaurant.image" alt="">
+            </div>
+            <p>{{ restaurant.name }}</p>
+          </div>
+          
+        </div>
+      </div>
+
     </div>
     <div class="banniere"></div>
     <restaurant-row v-for="(data, i) in data_restaurant" :key="i" :three_restaurant="data"/>
@@ -10,7 +23,6 @@
 </template>
 
 <script>
-/*eslint-disable*/
 //IMPORT
 import BDD from '../BDD'
 import { onMounted, ref, watch } from 'vue'
@@ -55,13 +67,15 @@ export default {
       }
       //user search restaurant
       let user_search_restaurant = ref('');
+      let search_restaurant = ref([]);
 
-      watch(user_search_restaurant, new_Value => {
+      watch(user_search_restaurant, new_value => {
 
-        let regex = RegExp(new_Value);
+        let regex = RegExp(new_value.toLowerCase());
 
-        let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name));
-        console.log(search_restaurant);
+        let new_search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name.toLowerCase()));
+
+        new_value==0 ? search_restaurant.value = [] : search_restaurant.value = new_search_restaurant;
 
       })
 
@@ -72,6 +86,7 @@ export default {
       return {
         data_restaurant,
         user_search_restaurant,
+        search_restaurant,
       }
     },
 }
@@ -90,13 +105,46 @@ export default {
       width: 200px;
     }
 
-    input{
-      background-color: #f6f6f6;
-      border: none;
-      height: 40px;
-      width: 400px;
-      outline: none;
-      padding: 0px 25px;
+    .wrapper--input {
+      position: relative;
+      input{
+        background-color: #f6f6f6;
+        border: none;
+        height: 40px;
+        width: 400px;
+        outline: none;
+        padding: 0px 25px;
+      }
+
+      .search {
+        position: absolute;
+        top: 100%;
+        width: 100%;
+        background-color: #ffffff;
+
+        .container--restaurant--search {
+          display: flex;
+          align-items: center;
+          padding: 10px;
+
+          &:hover {
+            background-color: #f6f6f6;
+          }
+
+          .wrapper--img {
+            height: 60px;
+            width: 60px;
+            margin-right: 25px;
+            border-radius: 50%;
+            overflow: hidden;
+
+            img {
+              height: 100%;
+              width: auto;
+            }
+          }
+        }
+      }
     }
   }
 
